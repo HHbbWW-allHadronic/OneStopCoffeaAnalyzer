548a9723c81504e334e0866ff6cc05794985dbde
Garath Vetters <gavetter@ttu.edu>
2026-03-05 14:54:12 -0600

    fixed a small error made with mean and std

--- analyzer/postprocessing/plots/plots_1d.py
+++ analyzer/postprocessing/plots/plots_1d.py
@@ -46,8 +46,10 @@ def plotOne(
         h = item.histogram
         if show_int:
             integral = round(h.sum().value)
-            mean = np.mean(h.values())
-            std = np.std(h.values())
+            counts = h.values()
+            centers = h.axes[0].centers
+            mean = np.average(centers, weights=counts)
+            std = np.sqrt(np.average((centers - mean)**2, weights=counts))
             title = f"{title}, N={integral}\nmean={mean:.3f}, std={std:.3f}"
         style = styler.getStyle(meta)
         h.plot1d(
@@ -72,8 +74,10 @@ def plotOne(
             title = meta.get("title") or meta["dataset_title"]
             if show_int:
                 integral = item.histogram.sum().value
-                mean = np.mean(item.histogram.values())
-                std = np.std(item.histogram.values())
+                counts = item.histogram.values()
+                centers = item.histogram.axes[0].centers
+                mean = np.average(centers, weights=counts)
+                std = np.sqrt(np.average((centers - mean)**2, weights=counts))
                 title = f"{title}, N={integral}\nmean={mean:.3f}, std={std:.3f}"
             titles.append(title)
             style = styler.getStyle(meta)
