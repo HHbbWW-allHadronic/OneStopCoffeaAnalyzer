36e02534ade674e0bae6b794405510749071bfe1
Joe Gehrke <gehrke@umn.edu>
2026-07-07 15:20:20 -0500

    Add 2D jet rank histos, implement for ABCD network inference, formatting

--- analyzer/modules/dihiggs/histograms.py
+++ analyzer/modules/dihiggs/histograms.py
@@ -83,16 +83,37 @@ class FourVecHistograms(AnalyzerModule):
     def inputs(self, metadata):
         return [self.input_col]
 
+
 @define
 class JetVarRankHistograms(AnalyzerModule):
+    """
+    Produce histograms of a variable for the first N jets in an event.
 
-
-    input_col: Column
+    Parameters
+    ----------
     hist_name: str
-
+        Name of column to be used in histogram. Can be formatted for 2D histograms with second variable names.
+    input_col : Column
+        Column containing the object collection (e.g. jets).
     axis: RegularAxis
-    max_idx: int = 6
+        Axis for the variable to be plotted.
+    second_names: list[str]
+        Names of the second variable to be plotted in 2D histograms (for hist name).
+    second_cols: list[Column]
+        Columns containing the second variable to be plotted in 2D histograms.
+    second_axes: list[RegularAxis]
+        Axes for the second variable to be plotted in 2D histograms.
+    max_idx: int
+        Largest jet index to be plotted (default is 6).
+    """
 
+    hist_name: str
+    input_col: Column
+    axis: RegularAxis
+    second_names: list[str] = []
+    second_cols: list[Column] = []
+    second_axes: list[RegularAxis] = []
+    max_idx: int = 6
 
     def run(self, columns, params):
         var = columns[self.input_col]
@@ -101,29 +122,41 @@ class JetVarRankHistograms(AnalyzerModule):
         for i in range(0, self.max_idx):
             mask = ak.num(var, axis=1) > i
             jet_individual = padded[:, i]
-            
-
             rank_label = f"$_{{{i + 1}}}$"
-            
             new_name = f"{self.axis.name} {rank_label}"
 
             axis = evolve(self.axis, name=new_name)
             # Generate the histogram for Jet [i+1]
-            ret.append(
-                makeHistogram(
-                    f"{self.axis.name}{i+1}",
-                    columns,
-                    axis,
-                    jet_individual,
-                    description=f"{self.axis.name} of jet {i + 1}",
-                    mask=mask,
+            if not self.second_names:
+                ret.append(
+                    makeHistogram(
+                        f"{self.hist_name}{i+1}",
+                        columns,
+                        axis,
+                        jet_individual,
+                        description=f"{self.axis.name} of jet {i + 1}",
+                        mask=mask,
+                    )
                 )
-            )
-
+            else:
+                for j in range(len(self.second_names)):
+                    hist_name = self.hist_name.format(f"{i+1}_{self.second_names[j]}")
+                    second_var = columns[self.second_cols[j]]
+                    second_axis = self.second_axes[j]
+                    ret.append(
+                        makeHistogram(
+                            hist_name,
+                            columns,
+                            [axis, second_axis],
+                            [jet_individual, second_var],
+                            description=f"2d {self.axis.name} of jet {i + 1} and {second_axis.name}",
+                            mask=mask,
+                        )
+                    )
         return columns, ret
 
     def outputs(self, metadata):
         return []
 
     def inputs(self, metadata):
-        return [self.input_col]
+        return [self.input_col] + self.second_cols
