2e803ceae487cb57d53c4058899855a2d25735b5
Joe Gehrke <gehrke@umn.edu>
2026-07-22 12:49:22 -0500

    Add IntCategory Axis type, modify cattrs to configure properly (hopefully)

--- analyzer/modules/common/axis.py
+++ analyzer/modules/common/axis.py
@@ -1,7 +1,9 @@
 from attrs import define
 import abc
 import hist
+import functools as ft
 from cattrs.strategies import include_subclasses
+from cattrs.strategies import configure_tagged_union
 
 
 @define(frozen=True)
@@ -16,9 +18,10 @@ class IntegerAxis(Axis):
     start: int
     stop: int
     unit: str | None = None
+    growth: bool = False
 
     def toHist(self):
-        a = hist.axis.Integer(self.start, self.stop, name=self.name)
+        a = hist.axis.Integer(self.start, self.stop, name=self.name, growth=self.growth)
         if self.unit:
             a.unit = self.unit
         return a
@@ -40,6 +43,19 @@ class RegularAxis(Axis):
 
 
 @define(frozen=True)
+class IntCategoryAxis(Axis):
+    name: str
+    categories: list[int]
+    unit: str | None = None
+
+    def toHist(self):
+        a = hist.axis.IntCategory(self.categories, name=self.name)
+        if self.unit:
+            a.unit = self.unit
+        return a
+
+
+@define(frozen=True)
 class VariableAxis(Axis):
     edges: list[float | tuple[float, float, int]]
     name: str
@@ -49,7 +65,7 @@ class VariableAxis(Axis):
         import numpy as np
 
         def toList(x):
-            if isinstance(x,tuple):
+            if isinstance(x, tuple):
                 return list(float(y) for y in np.arange(*x))
             else:
                 return [x]
@@ -61,6 +77,12 @@ class VariableAxis(Axis):
             a.unit = self.unit
         return a
 
+
 def configureConverter(conv):
     # union_strategy = ft.partial(configure_tagged_union, tag_name="module_name")
-    include_subclasses(Axis, conv)  # , union_strategy=union_strategy)
+    include_subclasses(
+        Axis,
+        conv,
+        union_strategy=ft.partial(configure_tagged_union, tag_name="module_name"),
+    )
+    # include_subclasses(Axis, conv)  # , union_strategy=union_strategy)
