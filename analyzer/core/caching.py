74c1d3187472f96ba5e4d152fb1a02b9ce3720bd
Joe Gehrke <gehrke@umn.edu>
2026-08-24 13:08:11 -0500

    Update caching for multi-node running

--- analyzer/core/caching.py
+++ analyzer/core/caching.py
@@ -1,5 +1,11 @@
 from diskcache import Cache
 from analyzer.configuration import CONFIG
 from pathlib import Path
+import socket
 
-cache = Cache(directory=Path(CONFIG.general.base_data_path) / CONFIG.cache.cache_subdir)
+cache = Cache(
+    directory=Path(CONFIG.general.base_data_path)
+    / CONFIG.cache.cache_subdir
+    / socket.gethostname(),
+    sqlite_journal_mode="delete",
+)
